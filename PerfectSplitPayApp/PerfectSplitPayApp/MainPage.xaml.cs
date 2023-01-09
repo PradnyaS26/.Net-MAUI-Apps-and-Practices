2c96b8d5e0adaf3e09c09698f487bc409e042b84
Pradnya Suryavanshi <suryavanshipradnya@gmail.com>
2023-01-09 16:05:13 +0530

    Updated Project Files

--- PerfectSplitPayApp/PerfectSplitPayApp/MainPage.xaml.cs
+++ PerfectSplitPayApp/PerfectSplitPayApp/MainPage.xaml.cs
@@ -2,24 +2,63 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    decimal bill;
+    int tip, noPersons = 1;
 
-	public MainPage()
+
+    public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void CalculateTotal()
+    {
+        var totalTip = (bill * tip) / 100;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        var tipByPerson = (totalTip / noPersons);
+        tipByPerson = decimal.Round(tipByPerson, 2, MidpointRounding.AwayFromZero);
+        lblTipByPerson.Text = $"{tipByPerson}";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        var subtotal = (bill / noPersons);
+        subtotal = decimal.Round(subtotal, 2, MidpointRounding.AwayFromZero);
+        lblSubtotal.Text = $"{subtotal}";
+
+        var totalByPerson = ((bill + totalTip) / noPersons);
+        totalByPerson = decimal.Round(totalByPerson, 2, MidpointRounding.AwayFromZero);
+        lblTotal.Text = $"{totalByPerson}";
+
+    }
+    void txtBill_Completed(System.Object sender, System.EventArgs e)
+    {
+        if (txtBill.Text != "")
+        {
+            bill = decimal.Parse(txtBill.Text);
+            CalculateTotal();
+        }
+    }
+
+    void sldTip_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+    {
+        tip = (int)sldTip.Value;
+        lblTip.Text = $"Tip : {tip}%";
+        CalculateTotal();
+    }
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if (sender is Button)
+        {
+            var btn = (Button)sender;
+            var percentage = int.Parse(btn.Text.Replace("%", ""));
+            sldTip.Value = percentage;
+        }
+    }
+
+    void stpSplit_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+    {
+        noPersons = (int)stpSplit.Value;
+        CalculateTotal();
+    }
 }
 
 
