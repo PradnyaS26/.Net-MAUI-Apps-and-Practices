385749f27f13276d5438480a5cd401bebc746c3e
Pradnya Suryavanshi <suryavanshipradnya@gmail.com>
2023-01-10 12:17:39 +0530

    some changes

--- PerfectSplitPayApp/PerfectSplitPayApp/App.xaml.cs
+++ PerfectSplitPayApp/PerfectSplitPayApp/App.xaml.cs
@@ -8,7 +8,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-        AppCenter.Start("ios=0d6aba52 - 2672 - 4cf4 - aab0 - 56c1c6ab4956 ;macos=0d6aba52 - 2672 - 4cf4 - aab0 - 56c1c6ab4956 ;android=0d6aba52 - 2672 - 4cf4 - aab0 - 56c1c6ab4956  ", typeof(Analytics), typeof(Crashes));
+        //AppCenter.Start("ios=0d6aba52 - 2672 - 4cf4 - aab0 - 56c1c6ab4956 ;macos=0d6aba52 - 2672 - 4cf4 - aab0 - 56c1c6ab4956 ;android=0d6aba52 - 2672 - 4cf4 - aab0 - 56c1c6ab4956  ", typeof(Analytics), typeof(Crashes));
         MainPage = new MainPage();
 	}
 }
